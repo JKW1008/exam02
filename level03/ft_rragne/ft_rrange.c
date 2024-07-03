fa23826f4665f929c4a799b108b6be7684c18f08
JKW1008 <todendvv91@gmail.com>
2024-07-04 03:10:53 +0900

    ft_rrange

---
+++ level03/ft_rragne/ft_rrange.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+int *ft_rrange(int start, int end)
+{
+	int *range;
+	int i = 0;
+	int step = 1;
+	int n = end - start;
+	
+	if (n < 0)
+		(n *= -1);
+	range = (int *)malloc(sizeof(int) * (n + 1));
+	if (range)
+	{
+		if (start < end)
+			step = -1;
+		while (i < n)
+		{
+			range[i] = end;
+			end = end + step;
+			i++;
+		}
+	}
+	return (range);
+}
